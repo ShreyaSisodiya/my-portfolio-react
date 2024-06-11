73e32075887ad434295e1c9743987037b377b87a
ShreyaSisodiya <98209442+ShreyaSisodiya@users.noreply.github.com>
2024-06-11 19:23:20 -0400

    for deployment

---
+++ my-portfolio/src/app/components/Skills.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const skills = [
+    "Java", "Spring Boot", "HTML", "CSS", "JavaScript", "TypeScript", "Python", "C++", "SQL",
+    "MongoDB", "React", "Next.js", "shadcn", "Node.js", "Tailwind", "REST API", "Redis",
+    "Redux", "GraphQL", "Express", "PostgreSQL", "Django", "Framer Motion", "Talend", "Selenium",
+    "Swing", "Flask", "Bootstrap", "Pandas", "Numpy", "Matplotlib", "Alteryx", "GCP BigQuery",
+    "AWS", "Docker", "Kubernetes", "Jenkins", "Figma", "Git", "Power BI", "Tableau"
+];
+
+const Skills = () => {
+    return (
+        <section id="skills" className="mb-28 mt-30 sm:mb-40 ">
+            <div className="container mx-auto max-w-[53rem] my-20 text-center scroll-mt-28">
+                <h2 className="text-3xl font-medium capitalize mb-8">
+                    Tools and Technologies which help me help you!
+                </h2>
+                <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800 dark:text-white/80">
+                    {skills.map((skill, index) => (
+                        <li
+                            key={index}
+                            className="bg-white border-2 border-black rounded-full px-6 py-3 text-xl cursor-pointer dark:bg-white/10"
+                        >
+                            {skill}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    );
+};
+
+export default Skills;
