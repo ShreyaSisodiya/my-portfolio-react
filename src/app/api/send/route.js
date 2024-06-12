fa86d656fb8081924be63b9f38511876e5fd72bd
ShreyaSisodiya <98209442+ShreyaSisodiya@users.noreply.github.com>
2024-06-11 23:54:10 -0400

    email

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,29 +1,42 @@
-// import { NextResponse } from "next/server";
-// import { Resend } from "resend";
+import { NextResponse } from "next/server";
 
-// const resend = new Resend(process.env.RESEND_API_KEY);
-// const fromEmail = process.env.FROM_EMAIL;
+const nodemailer = require('nodemailer');
+require('dotenv').config();
 
-// export async function POST(req, res) {
-//     const { email, subject, message } = await req.json();
-//     console.log("Hello" + email, subject, message, fromEmail);
-//     try {
-//         const data = await resend.emails.send({
-//             from: fromEmail,
-//             to: [fromEmail, email],
-//             subject: subject,
-//             react: (
-//                 <>
-//                     <h1>{subject}</h1>
-//                     <p>Thank you for contacting me!</p>
-//                     <p>New message submitted:</p>
-//                     <p>{message}</p>
-//                 </>
-//             ),
-//         });
-//         return NextResponse.json("status: " + data);
-//     } catch (error) {
-//         console.log("Error found: " + error.message);
-//         return NextResponse.json({ error });
-//     }
-// }
+export async function POST(req, res) {
+    const { email, subject, message } = await req.json();
+
+
+    try {
+        let transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS
+            }
+        });
+
+        // Set up email data
+        let mailOptions = {
+            from: `"Your Name" <${process.env.SMTP_USER}>`, // Sender address
+            to: 'sisodiya.s@northeastern.edu', // List of recipients
+            subject: subject, // Subject line
+            text: email + "/n" + message, // Plain text body
+        };
+
+        // Send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+        });
+
+        return NextResponse.json("status: " + data);
+
+    } catch (error) {
+
+    }
+}
